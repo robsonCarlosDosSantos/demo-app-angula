384318957d6d8a6203e9531b8c459c4e66b0d6d7
Robson Carlos <rsc.robson.carlos.santos@gmail.com>
2020-05-24 15:46:33 +0100

    Internacionalização -> Formato moeda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { registerLocaleData } from '@angular/common';
+import localePTBR from '@angular/common/locales/pt';
 
 import { ROUTES } from './app.routes';
 
@@ -14,6 +16,8 @@ import { DiversaoComponent } from './diversao/diversao.component';
 import { OfertaComponent } from './oferta/oferta.component';
 import { ComoUsarComponent } from './oferta/como-usar/como-usar.component';
 import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component';
+
+registerLocaleData(localePTBR, 'pt');
 
 @NgModule({
   declarations: [
@@ -32,7 +36,7 @@ import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component';
     HttpClientModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [],
+  providers: [{provide: LOCALE_ID, useValue: 'pt-Br' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
